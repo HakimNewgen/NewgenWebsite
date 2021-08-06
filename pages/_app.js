78075b9c1a3c4445255d93adf8cbb51df03fdbc0
HakimNewgen <87693425+HakimNewgen@users.noreply.github.com>
2021-08-06 17:04:00 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,8 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+      <Head> 
+           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
     </>
